43b32d87a07e7ba51a50e54a1205c4fbba2b4ab5
Ashkboos <mahdihossieni@gmail.com>
2020-09-20 17:38:26 +0430

    Side Drawer added and made responsve

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Div from '../../hoc/Div';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = props => {
   return (
     <Div>
       <Toolbar />
+      <SideDrawer />
       <main className={classes.Content}>{props.children}</main>
     </Div>
   );
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
     <div>Menu</div>
-    <div>LOGO</div>
-    <nav>...</nav>
+    <Logo height="80%" />
+    <nav className={classes.DesktopOnly}>
+      <NavigationItems />
+    </nav>
   </header>
 );
 
