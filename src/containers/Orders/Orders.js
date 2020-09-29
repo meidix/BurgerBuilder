05bf16d5986eb8bf020862796c4ce0a572163a00
Mehdi Hossieni <mahdihossieni@gmail.com>
2020-09-29 10:42:19 +0330

    little bug in orders component

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,27 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Order from '../../components/Order/Order';
-import axios from '../../axios-orders';
-import withErrorHandler from '../../hoc/ErrorHadler/withErrorHandler';
+import Order from "../../components/Order/Order";
+import axios from "../../axios-orders";
+import withErrorHandler from "../../hoc/ErrorHadler/withErrorHandler";
+import Spinner from "../../components/UI/Spinner/Spinner";
 
 class Orders extends Component {
   state = {
     orders: [],
+    loading: true,
   };
   componentDidMount() {
-    axios.get('/orders.json').then(response => {
+    axios.get("/orders.json").then((response) => {
       const fetchOrders = [];
       for (let key in response.data) {
         fetchOrders.push({ ...response.data[key], id: key });
       }
-      this.setState({ orders: fetchOrders });
+      this.setState({ orders: fetchOrders, loading: false });
     });
   }
 
   render() {
+    if (this.state.loading) {
+      return <Spinner />;
+    }
     return (
       <div>
-        {this.state.orders.map(order => (
+        {this.state.orders.map((order) => (
           <Order
             key={order.id}
             ingredients={order.ingredients}
