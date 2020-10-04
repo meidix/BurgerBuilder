781138eee75361124c7f8e17c220ccc4dd707c33
Ashkboos <mahdihossieni@gmail.com>
2020-10-04 11:52:54 +0330

    sign in and signup pages are going to be added

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Auth extends Component {
+  state = {
+    controls: {},
+  };
+
+  render() {
+    return (
+      <div>
+        <form></form>
+      </div>
+    );
+  }
+}
+
+export default Auth;
